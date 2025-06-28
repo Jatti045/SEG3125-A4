2b6aee65daca4b2d380d727c32e0c802a469abf9
ejfattouch <ejfattouch@outlook.com>
2025-06-27 20:48:28 -0400

    Removed banners

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Footer from "../../components/ui/Footer";
 import Navbar from "../../components/ui/Navbar";
-import Promo from "../../components/home/Promo";
 
 const Home = () => {
   return (
     <>
       <div className="min-h-screen">
         <Navbar />
-        <Promo />
       </div>
       <div>
         <Footer />
